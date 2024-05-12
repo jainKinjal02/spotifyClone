a95d14625cf92f30d2e326859f3ec8b5a97afd20
Kinjal Jain <jainkinjal021196@gmail.com>
2024-05-12 17:26:14 +0530

    removed Loader

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <div className="relative flex">
       {/*<Sidebar />*/}
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#861212]">
-        <Searchbar />
+        {/*<Searchbar />*/}
 
         <div className="px-6 h-[calc(100vh-20px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
